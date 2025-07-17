b9393ebd48e1047ac2ee43cd7fae803c46fcb5d6
thanawat004 <tipidokthanawat@gmail.comgit config --global user.email tipidokthanawat@gmail.com>
2025-07-17 11:38:54 +0700

    week-3 commit

---
+++ week-3/ex1.py
@@ -0,0 +1,10 @@
+decimal = input("input decimal here : ")
+decimal = int(decimal)
+
+binary = bin(decimal)
+hex = hex(decimal)
+oct = oct(decimal)
+
+print("this is binery ",binary)
+print("this is hex ",hex)
+print("this is oct ",oct)
